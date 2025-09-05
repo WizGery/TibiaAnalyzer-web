8e72534ec1cfa180f70bf66a0ce0034249bc3383
WizGery <40678575+WizGery@users.noreply.github.com>
2025-09-05 02:06:03 +0200

    test

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,8 +19,8 @@ pages = [
     st.Page("app_pages/0_Login.py",            title="Account",       icon="🔐"),
     st.Page("app_pages/1_Zone_Averages.py",    title="Zone Averages", icon="📊"),
     st.Page("app_pages/4_Statistics.py",       title="Statistics",    icon="📈"),
-    st.Page("app_pages/_debug_auth.py",       title="debug",    icon="📈"),
-    st.Page("app_pages/8_Debug.py", title="Debug", icon="🛠️"),
+    st.Page("app_pages/_debug_auth.py",        title="debug",         icon="📈"),
+    st.Page("app_pages/8_Debug.py",            title="Debug",         icon="🛠️"),
 ]
 
 if is_logged_in():
